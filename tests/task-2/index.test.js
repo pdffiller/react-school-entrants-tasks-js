cba68a2be4bf29da453e916a22a0c938da60bacd
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2018-06-14 16:32:13 +0300

    [dsch] Task 2 - added tests on side-effects detection

--- tests/task-2/index.test.js
+++ tests/task-2/index.test.js
@@ -3,7 +3,6 @@ import haveSameItems from '../../task-2';
 import cases from './__fixtures__';
 
 describe('Task 2 - Have Same Items', () => {
-
   const runTest = caseName => test(caseName, () => {
     const [first, second, result] = cases[caseName];
     expect(
@@ -11,5 +10,13 @@ describe('Task 2 - Have Same Items', () => {
     ).toBe(result);
   });
 
+  const runFrozenTest = caseName => test(`${caseName} - frozen`, () => {
+    const [first, second, result] = cases[caseName];
+    expect(
+      haveSameItems(Object.freeze(first), Object.freeze(second))
+    ).toBe(result);
+  });
+
   Object.keys(cases).forEach(runTest);
+  Object.keys(cases).slice(-2).forEach(runFrozenTest);
 });
